c52bdd407aee70e272c643e120e2e85eece24357
choieastsea <choieastsea@gmail.com>
2023-07-31 19:51:50 +0900

    [FIX]

--- acmipc/1504_특정한최단경로/1504.py
+++ acmipc/1504_특정한최단경로/1504.py
@@ -11,7 +11,7 @@ def dijkstra(start, target):
     global V
     visited = [False for _ in range(V)]
     path = [INF for _ in range(V)]
-    path[start] = 0
+    path[start] = 0 # 출발지는 0!! (이거 까먹어서 틀린듯)
     pq = []
     heappush(pq, (0, start)) # (거리, 노드번호)
     while len(pq) > 0:
@@ -44,8 +44,8 @@ if __name__ == "__main__":
     d4 = dijkstra(0,v2-1)
     d5 = dijkstra(v1-1, V-1)
 
-    print(d1,d2,d3)
-    print(d4,d2,d5)
+    # print(d1,d2,d3)
+    # print(d4,d2,d5)
 
     if d1 == INF or d2 == INF or d3 == INF or d4 == INF or d5 == INF:
         print(-1)
